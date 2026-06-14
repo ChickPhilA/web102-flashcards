dd0f0196ee9565286e59930648d0831f73db2892
Phil <natsudragoneel282003@gmail.com>
2026-06-13 18:30:40 -0700

    Created a data array of flashcards of coffee, along with a state of a starting random flashcard index when you load the webpage

--- src/App.jsx
+++ src/App.jsx
@@ -1,121 +1,33 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
+import Flashcard from './components/Flashcard.jsx'
 import './App.css'
 
+const cards = [
+  {question: "Made from a brewer and a filter. The simple and traditional way", answer: "Coffee (black)"},
+  {question: "Pulled under intense pressure of hot water through a puck-shape of coffee grounds", answer: "Espresso"},
+  {question: "Espresso + steamed milk + crema foam", answer: "Latte"},
+  {question: "Chocolate sauce + espresso + steamed milk + crema foam", answer: "Mocha"},
+  {question: "Shot(s) of espresso diluted with a body of steamed water", answer: "Americano"},
+  {question: "Coffee grounds brewed immediately over ice", answer: "Iced Coffee"},
+  {question: "Concentrated coffee grounds brewed in cold water for around 16-20 hours, without direct sunlight", answer: "Cold Brew"},
+  {question: "An upside-down vanilla latte, where vanilla is layered with steamed milk, foam, espresso shots, and a caramel crosshatch drizzle.", answer: "Caramel Macchiato"},
+  {question: "Double ristretto espresso shots + steamed whole milk, and a thin microfoam layer on top", answer: "Flat White"}
+]
+
 function App() {
-  const [count, setCount] = useState(0)
+
+  let startingPoint = Math.floor(Math.random() * cards.length) // the starting index of the flashcard it'll be randomized to
+  const [currentIndex, setCurrentIndex] = useState(startingPoint)
 
   return (
-    <>
-      <section id="center">
-        <div className="hero">
-          <img src={heroImg} className="base" width="170" height="179" alt="" />
-          <img src={reactLogo} className="framework" alt="React logo" />
-          <img src={viteLogo} className="vite" alt="Vite logo" />
-        </div>
-        <div>
-          <h1>Get started</h1>
-          <p>
-            Edit <code>src/App.jsx</code> and save to test <code>HMR</code>
-          </p>
-        </div>
-        <button
-          type="button"
-          className="counter"
-          onClick={() => setCount((count) => count + 1)}
-        >
-          Count is {count}
-        </button>
-      </section>
+    <div>
+      <h1 className="title"> Coffee-Break Study</h1>
+      <h3> Think you're a coffee expert? Test yourself against these flashcards and these
+        build of coffee drinks. </h3>
+      <h4> Total number of cards: {cards.length} </h4>
 
-      <div className="ticks"></div>
-
-      <section id="next-steps">
-        <div id="docs">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#documentation-icon"></use>
-          </svg>
-          <h2>Documentation</h2>
-          <p>Your questions, answered</p>
-          <ul>
-            <li>
-              <a href="https://vite.dev/" target="_blank">
-                <img className="logo" src={viteLogo} alt="" />
-                Explore Vite
-              </a>
-            </li>
-            <li>
-              <a href="https://react.dev/" target="_blank">
-                <img className="button-icon" src={reactLogo} alt="" />
-                Learn more
-              </a>
-            </li>
-          </ul>
-        </div>
-        <div id="social">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#social-icon"></use>
-          </svg>
-          <h2>Connect with us</h2>
-          <p>Join the Vite community</p>
-          <ul>
-            <li>
-              <a href="https://github.com/vitejs/vite" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#github-icon"></use>
-                </svg>
-                GitHub
-              </a>
-            </li>
-            <li>
-              <a href="https://chat.vite.dev/" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#discord-icon"></use>
-                </svg>
-                Discord
-              </a>
-            </li>
-            <li>
-              <a href="https://x.com/vite_js" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#x-icon"></use>
-                </svg>
-                X.com
-              </a>
-            </li>
-            <li>
-              <a href="https://bsky.app/profile/vite.dev" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#bluesky-icon"></use>
-                </svg>
-                Bluesky
-              </a>
-            </li>
-          </ul>
-        </div>
-      </section>
-
-      <div className="ticks"></div>
-      <section id="spacer"></section>
-    </>
+      <Flashcard question={cards[currentIndex].question} answer={cards[currentIndex.answer]} />
+    </div>
   )
 }
 
