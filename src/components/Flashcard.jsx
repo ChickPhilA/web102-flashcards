4776205a36638eb9e78de47f5d29c629fb6d3595
Phil <natsudragoneel282003@gmail.com>
2026-06-20 22:41:29 -0700

    Added comment to fix flippedBack state in future commits

--- src/components/Flashcard.jsx
+++ src/components/Flashcard.jsx
@@ -4,11 +4,11 @@ import '../App.css'
 const Flashcard = ({question, answer}) => {
 
     const [isFlippedToBack, setFlippedToBack] = useState(false) // all cards will always start off on the front by default
+    // One thing to fix: when to the next or previous card, always set this to false.
 
     return(
         <div className="card" onClick={() => setFlippedToBack(!isFlippedToBack)}>
             <div className={`inner ${isFlippedToBack ? "flipped" : ""}`}>
-
                 <div className="front">
                     {question}
                 </div>
